cdd82f95bd2467197a964adc6059bb4ff44e6b27
Matt Knopp <matt.knopp@gmail.com>
2013-09-20 16:02:52 -0700

    wip

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsMesosDockerSample::Application.config.secret_key_base = '55c3d5d5b7afff4e72f403810113ba75077d6683d9517e0c0cea85e6167e0a630403a98ce34300a4a1153decf16ee04c8904cde006b85ecf170067f80aae8eb1'
